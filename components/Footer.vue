13fbd30b09c35662df26e034d0f607709598d7c4
ConstanceHermit <hello@constancehermit.com>
2022-09-09 13:06:43 +0200

    Refactors Privacy Page

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
     <footer class="w-full relative bottom-0 text-center mt-12">
        	<span class="">&copy; 2022 Constance Armitage</span> | 
-        <span class="prose"><a href="/privacy/policy">Privacy Policy</a></span>
+        <span class="prose"><a href="/privacy">Privacy</a></span> |
+        <span class="prose"><a href="/legal">Legal</a></span>
     </footer>
 </template>
---
+++ pages/privacy.vue
@@ -0,0 +1,29 @@
+<template>
+    <article>
+      <div class="w-full grid grid-cols-3 col-span-3 col-start-1 "
+            v-for="policy in policy" :key="policy.title">
+        <div class="w-full col-span-3 col-start-1 px-4">
+          <h1 class="text-4xl py-4">{{policy.title}}</h1>
+          <span>Last updated: {{ formatDate(policy.updatedAt) }}</span>
+          <nuxt-content class="mt-4 prose max-w-none" :document="policy" />
+        </div>
+      </div>
+    </article>
+</template>
+
+<script>
+
+export default {
+    async asyncData({ $content, params }) {
+        const policy = await $content('privacy', params.slug).fetch()
+        console.log("HI" + policy.title)
+        return { policy }
+    },
+    methods: {
+        formatDate(date) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' }
+        return new Date(date).toLocaleDateString('en-gb', options)
+        }
+    }
+}
+</script>
