5352b9b09f9cf1af21fe1c7b4db0b6cd6038bc05
ConstanceHermit <hello@constancehermit.com>
2022-09-13 11:39:31 +0200

    Adjusts Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,11 @@
 <template>
-    <footer class="w-full relative bottom-0 text-center mt-12">
-       	<span class="">&copy; 2022 Constance Armitage</span> | 
-        <span class="prose"><a href="/privacy">Privacy</a></span> |
-        <span class="prose"><a href="/legal">Legal</a></span>
+    <footer class="w-full relative bottom-0 ml-2 mb-2 mt-12
+                   text-center">
+        <span class="prose text-base">
+            Drawn To Code
+        </span><br/> 
+        <span class="prose text-xs">
+            <a href="/privacy">Privacy</a> & <a href="/legal">Legal</a> 
+        </span>
     </footer>
 </template>
