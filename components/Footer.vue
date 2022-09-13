a7bcd4a66ed580dc0b04bd76d47eb8eab29e1b08
ConstanceHermit <hello@constancehermit.com>
2022-09-13 11:04:44 +0200

    Adds some responsive styling
    and adjusts nav/footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
-    <footer class="w-full relative bottom-0 text-center mt-12">
-       	<span class="">&copy; 2022 Constance Armitage</span> | 
-        <span class="prose"><a href="/privacy">Privacy</a></span> |
-        <span class="prose"><a href="/legal">Legal</a></span>
+    <footer class="w-full relative bottom-0 mt-12 ml-4 pb-2">
+        <div class="prose">
+             <span class="text-lg font-semibold">Drawn To Code</span><br/>
+             <span class="text-sm">
+                <a href="/privacy">Privacy</a> & <a href="/legal">Legal</a>
+             </span>
+        </div>
     </footer>
 </template>
