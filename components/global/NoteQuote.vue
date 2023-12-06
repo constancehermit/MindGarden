8c3849e9f4b665ab7b0c0eef23768b1c0cd1dc34
Constance Armitage <hello@constancehermit.com>
2023-12-06 12:36:56 +0100

    Adds a custom quote component

---
+++ components/global/NoteQuote.vue
@@ -0,0 +1,35 @@
+<template>
+    <blockquote>
+        <span 
+            class="">
+            <b>{{ quote }}</b>
+        </span>
+        <br/>
+        <span 
+            class="text-sm text-slate-400/75 italic">
+            -- <a 
+                :href="sourcelink" class="text-sky-600/75">
+                {{sourcetext}}
+            </a>
+        </span>
+    </blockquote>
+</template>
+
+<script>
+export default {
+    props: {
+        quote: {
+            type: String,
+            required: true
+        },
+        sourcetext: {
+            type: String, 
+            required: false
+        },
+        sourcelink: {
+            type: String,
+            required: false
+        }
+    }
+}
+</script>
