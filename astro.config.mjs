9c6f52a4b84451f4646638b519c3ee1da1455424
Constance Armitage <constancehermit@drawntocode.com>
2025-11-19 18:40:10 +0100

    Adds astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
+import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
