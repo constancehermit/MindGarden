92bc01764cddcd1775d49027dee886428c9ad787
Constance Armitage <constancehermit@drawntocode.com>
2025-11-20 10:09:36 +0100

    Adds theme toggler & updates header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,23 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import icon from 'astro-icon';
-import tailwindcss from '@tailwindcss/vite';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  // TODO: update URL on merging feature branch
+  site: "https://example.com",
   integrations: [
     mdx(),
     sitemap(),
     icon({
       include: {
-        'simple-icons': ['github', 'bluesky', 'tumblr'],
+        "simple-icons": ["github", "bluesky", "tumblr"],
       },
-    })
+    }),
   ],
 
   vite: {
